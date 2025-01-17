d7b1af9724d2dfc7c6e5853c8d5c889300e2b2c2
emanuelugr <emanuelgh@correo.ugr.es>
2025-01-17 17:49:00 +0100

    Implementar división por 0 devuelva Nan

--- master-ugr.calculator.back-end/tests/calculator.backend.test/steps/CalculatorSteps.cs
+++ master-ugr.calculator.back-end/tests/calculator.backend.test/steps/CalculatorSteps.cs
@@ -11,76 +11,91 @@ using static System.Net.WebRequestMethods;
 
 namespace calculator.lib.test.steps
 {
-    [Binding]
-    public class CalculatorSteps
-    {
-        private readonly ScenarioContext _scenarioContext;
+	[Binding]
+	public class CalculatorSteps
+	{
+		private readonly ScenarioContext _scenarioContext;
 
-        public CalculatorSteps(ScenarioContext scenarioContext)
-        {
-            _scenarioContext = scenarioContext;
-        }
+		public CalculatorSteps(ScenarioContext scenarioContext)
+		{
+			_scenarioContext = scenarioContext;
+		}
 
-        [Given(@"the first number is (.*)")]
-        public void GivenTheFirstNumberIs(int firstNumber)
-        {
-            _scenarioContext.Add("firstNumber", firstNumber);
-        }
+		[Given(@"the first number is (.*)")]
+		public void GivenTheFirstNumberIs(int firstNumber)
+		{
+			_scenarioContext.Add("firstNumber", firstNumber);
+		}
 
-        [Given(@"the second number is (.*)")]
-        public void GivenTheSecondNumberIs(int secondNumber)
-        {
-            _scenarioContext.Add("secondNumber", secondNumber);
-        }
-        private void ApiCall(string operation)
-        {
-            using (var client = new HttpClient())
-            {
-                var urlBase = _scenarioContext.Get<string>("urlBase");
-                var firstNumber = _scenarioContext.Get<int>("firstNumber");
-                var secondNumber = _scenarioContext.Get<int>("secondNumber");
-                var url = $"{urlBase}api/Calculator/";
-                var api_call = $"{url}{operation}?a={firstNumber}&b={secondNumber}";
-                var response = client.GetAsync(api_call).Result;
-                response.EnsureSuccessStatusCode();
-                var responseBody = response.Content.ReadAsStringAsync().Result;
-                var jsonDocument = JsonDocument.Parse(responseBody);
-                var result = jsonDocument.RootElement.GetProperty("result").GetDouble();
-                _scenarioContext.Add("result", result);
-            }
-        }
+		[Given(@"the second number is (.*)")]
+		public void GivenTheSecondNumberIs(int secondNumber)
+		{
+			_scenarioContext.Add("secondNumber", secondNumber);
+		}
+		private void ApiCall(string operation)
+		{
+			using (var client = new HttpClient())
+			{
+				var urlBase = _scenarioContext.Get<string>("urlBase");
+				var firstNumber = _scenarioContext.Get<int>("firstNumber");
+				var secondNumber = _scenarioContext.Get<int>("secondNumber");
+				var url = $"{urlBase}api/Calculator/";
+				var api_call = $"{url}{operation}?a={firstNumber}&b={secondNumber}";
+				var response = client.GetAsync(api_call).Result;
+				response.EnsureSuccessStatusCode();
+				var responseBody = response.Content.ReadAsStringAsync().Result;
+				var jsonDocument = JsonDocument.Parse(responseBody);
+				var result = jsonDocument.RootElement.GetProperty("result");
 
-        [When(@"the two numbers are added")]
+				if (result.ValueKind == JsonValueKind.Number)
+					_scenarioContext.Add("result", result.GetDouble());
+				else if (result.ValueKind == JsonValueKind.String && result.GetString() == "NaN")
+					_scenarioContext.Add("result", double.NaN);
+				else
+					throw new InvalidOperationException("Unexpected result format.");
+			}
+		}
 
-        public void WhenTheTwoNumbersAreAdded()
-        {
-            ApiCall("add");
-        }
-        [When(@"I divide first number by second number")]
-        public void WhenIDivideFirstNumberBySecondNumber()
-        {
-            ApiCall("divide");
-        }
+		[When(@"the two numbers are added")]
 
-        [When(@"I multiply both numbers")]
-        public void WhenIMultiplyBothNumbers()
-        {
-            ApiCall("multiply");
-        }
+		public void WhenTheTwoNumbersAreAdded()
+		{
+			ApiCall("add");
+		}
+		[When(@"I divide first number by second number")]
+		[When(@"I divide both numbers")]
+		public void WhenIDivideFirstNumberBySecondNumber()
+		{
+			ApiCall("divide");
+		}
 
-        [When(@"I substract first number to second number")]
-        public void WhenISubstractFirstNumberToSecondNumber()
-        {
-            ApiCall("subtract");
-        }
+		[When(@"I multiply both numbers")]
+		public void WhenIMultiplyBothNumbers()
+		{
+			ApiCall("multiply");
+		}
 
-        [Then(@"the result should be (.*)")]
-        [Then(@"the result shall be (.*)")]
-        [Then(@"the result is (.*)")]
-        public void ThenTheResultShouldBe(double expectedResult)
-        {
-            var result = _scenarioContext.Get<double>("result");
-            Assert.Equal(expectedResult, result);
-        }
-    }
+		[When(@"I substract first number to second number")]
+		public void WhenISubstractFirstNumberToSecondNumber()
+		{
+			ApiCall("subtract");
+		}
+
+		[Then(@"the result should be (.*)")]
+		[Then(@"the result shall be (.*)")]
+		[Then(@"the result is (.*)")]
+		public void ThenTheResultShouldBe(double expectedResult)
+		{
+			var result = _scenarioContext.Get<double>("result");
+
+			if (double.IsNaN(expectedResult))
+			{
+				Assert.True(double.IsNaN(result));
+			}
+			else
+			{
+				Assert.Equal(expectedResult, result);
+			}
+		}
+	}
 }
